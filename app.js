ececd6e54aa6448cbb60181f7251f7d763cbe1c9
Piotr Sankiewicz <petsan@hotmail.com>
2019-10-29 11:00:55 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+//challange
+//ask the user a name
+//have a line of code that checks if the name is equal to kali
+
+var askName = window.prompt('give me a name please');
+
+if (askName.toLowerCase === 'kali') {
+  window.alert('you got it')
+} 
