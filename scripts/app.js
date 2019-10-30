877bd16a394f997d25c5a6a2959c877b94751185
Piotr Sankiewicz <petsan@hotmail.com>
2019-10-30 15:49:06 -0700

    prepared script for quesiton 7, PS

--- scripts/app.js
+++ scripts/app.js
@@ -73,60 +73,70 @@ var pointsEarned = 0;
   
 // }
 
+
 /////////////////////////////
-// Guess a number - Question 6
-var numberToGuess = Math.floor(Math.random() * Math.floor(Number(10)));
-var guessAttemptCount = 4; 
-var numGuessedByUser = NaN;
+// Find out their likes, Questions 1-5
+var i = 0;
 
-while (guessAttemptCount > 0) {
-  numGuessedByUser = prompt('You have ' + guessAttemptCount + ' tries to guess a number between 1 and 10');
-  numGuessedByUser = Number(numGuessedByUser);
+for (i = 0; i < things.length; ++i) {
+  var answer = prompt('Do you like ' + things[i] + '\nY/No?');
+  answer = answer.toLowerCase();
 
-  if (numberToGuess === numGuessedByUser) {
-    alert('Awesome! You got it!');
-    guessAttemptCount = 0;
-    pointsEarned++;
-
-  } else if (numGuessedByUser < numberToGuess) {
-    alert(numGuessedByUser + 'is too low');
-    guessAttemptCount--;
-
-  } else if (numGuessedByUser > numberToGuess) {
-    alert(numGuessedByUser + 'is too high');
-    guessAttemptCount--;
+  if (answer === 'yes' || answer === 'y') {
+    // console.log('You like ' + things[i]);
+    alert('You like ' + things[i]);
+    
+    thingsLiked = thingsLiked + (things[i] + ' ');
+  } else if (answer === 'no' || answer === 'n') {
+    // console.log(answer + 'is notYou don\'t like ' + things[i]);
+    alert('You don\'t like ' + things[i]);
+    thingsDisliked = thingsDisliked + (things[i] + ' ');
+  } else {
+    // console.log(answer + ' is not the answer we were looking for');
+    alert(answer + ' is not the answer we were looking for');
   }
 }
 
 /////////////////////////////
-// var i = 0;
+// Guess a number - Question 6
+var numberToGuess = Math.floor(Math.random() * Math.floor(Number(10)));
+var guessAttemptCount = 4;
+var numberGuessedByUser = NaN;
 
-// for (i = 0; i < things.length; ++i) {
-//   var answer = prompt('Do you like ' + things[i] + '\nY/No?');
-//   answer = answer.toLowerCase();
+while (guessAttemptCount > 0) {
+  numberGuessedByUser = prompt('You have ' + guessAttemptCount + ' tries to guess a number between 1 and 10');
+  numberGuessedByUser = Number(numberGuessedByUser);
 
-//   if (answer === 'yes' || answer === 'y') {
-//     // console.log('You like ' + things[i]);
-//     alert('You like ' + things[i]);
-    
-//     thingsLiked = thingsLiked + (things[i] + ' ');
-//   } else if (answer === 'no' || answer === 'n') {
-//     // console.log(answer + 'is notYou don\'t like ' + things[i]);
-//     alert('You don\'t like ' + things[i]);
-//     thingsDisliked = thingsDisliked + (things[i] + ' ');
-//   } else {
-//     // console.log(answer + ' is not the answer we were looking for');
-//     alert(answer + ' is not the answer we were looking for');
-//   }
-// }
+  if (numberToGuess === numberGuessedByUser) {
+    alert('Awesome! You got it!');
+    guessAttemptCount = -1;
+    pointsEarned++;
+  } else if (numberGuessedByUser < numberToGuess) {
+    alert(numberGuessedByUser + ' is too low');
+    guessAttemptCount--;
+  } else if (numberGuessedByUser > numberToGuess) {
+    alert(numberGuessedByUser + ' is too high');
+    guessAttemptCount--;
+  } else { //THERE IS A BUG HERE, THIS LINE NEVER DISPLAYS
+    alert('Sorry, the correct answer was ' + numberToGuess);
+  }
+}
 
-// var userName = prompt('What is your name?');
+////////////////////
+//Question 7
 
-// console.log('Hello ' + userName + '!\nWelcome to our site!!');
-// alert('Hello ' + userName + '!\nWelcome to our site!!');
-
-// console.log('Things About ' + userName + '\n\nYou like: ' + thingsLiked + '\nYou don\'t like: ' + thingsDisliked);
-// alert('Things About ' + userName + '\n\nYou like: ' + thingsLiked + '\nYou don\'t like: ' + thingsDisliked);
 
 
 alert('You guessed ' + pointsEarned + ' questions correctly!!!');
+
+////////////////////
+//Ask for User's Name and display Greeting
+var userName = prompt('What is your name?');
+
+console.log('Hello ' + userName + '!\nWelcome to our site!!');
+alert('Hello ' + userName + '!\nWelcome to our site!!');
+
+console.log('Things About ' + userName + '\n\nYou like: ' + thingsLiked + '\nYou don\'t like: ' + thingsDisliked);
+alert('Things About ' + userName + '\n\nYou like: ' + thingsLiked + '\nYou don\'t like: ' + thingsDisliked);
+
+
